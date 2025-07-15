b7d31ea9dd95f4594ddc0002379d6ad7cc3a5d48
richaDev06 <itsricha4work@gmail.com>
2025-07-15 19:36:35 +0530

    Initial Commit

---
+++ HospitalProject/Hospital-Management-Html-master/ambu-call/script.js
@@ -0,0 +1,24 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+var counter = document.querySelector(".counter");
+
+let count = 1;
+
+setInterval(() => {
+    if(count < 100000){
+        count++;
+    counter.innerHTML = count
+    }
+}, 1000)
+
+fetch("AIzaSyAJ16M03Q0vdQZq9LxOSmfErmai-MgcOKk")
+    .then(res => res.json)
+    .then(data => console.log(data))
